bb060b00106ba99d0b0471a032f2c6bb9e7ed860
Victor Chao <Fireball332@outlook.com>
2018-07-09 21:12:49 +0800

    d

--- player.py
+++ player.py
@@ -9,7 +9,7 @@ class Player(pygame.sprite.Sprite):
 	    super().__init__()
 
 	    self.image = pygame.Surface([width, height])
-        self.image.fill(WHITE)
+        self.image.fill(WHITE )
         self.image.set_colorkey(WHITE)
 
         self.image = pygame.image.load("Things/Pictures/square.png")
